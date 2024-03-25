d27c53df22dd2773000a3ebe65478a043aaae0f5
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-25 03:42:20 -0700

    Add Data, Schema, SQL

---
+++ mysql.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLES
+-- POPULATE DATABASE
+-- QUERIES
+    -- Display all contents of the Clients table
+    -- First names, last names, ages and occupations of all clients
+    -- First and last names of clients that borrowed books in March 2018
+    -- First and last names of the top 5 authors clients borrowed in 2017
+    -- Nationalities of the least 5 authors that clients borrowed during the years 2015-2017
+    -- The book that was most borrowed during the years 2015-2017
+    -- Top borrowed genres for client born in years 1970-1980
+    -- Top 5 occupations that borrowed the most in 2016
+    -- Average number of borrowed books by job title
+    -- Create a VIEW and display the titles that were borrowed by at least 20% of clients
+    -- The top month of borrows in 2017
+    -- Average number of borrows by age
+    -- The oldest and the youngest clients of the library
+    -- First and last names of authors that wrote books in more than one genre
